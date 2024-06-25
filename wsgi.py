67922bd6593de4502d526da147908e506b482d8b
Kharee Smith <kharee.smith@yahoo.com>
2024-06-25 14:47:01 -0400

    Updated app structure for Heroku deployment

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,9 @@
 # Entry point for running the PhraseMaster Flask application
 from app import create_app
-import os
 
 # Create the Flask application instance
 app = create_app()
 
 # Run the application
 if __name__ == "__main__":
-    if os.getenv('FLASK_ENV') == 'development':
-        from waitress import serve
-        serve(app, host='0.0.0.0', port=5000)
-    else:
-        app.run(host='0.0.0.0', port=5000)
+        app.run()
